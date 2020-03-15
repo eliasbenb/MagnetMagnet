2a70092919e4272e80b13e8a5e3bc035129a6a68
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-03-15 18:33:43 +0400

    Made app store files in %APPDATA% to increase efficiency

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from tkinter import Tk, messagebox, StringVar, Label, Entry, Button, ttk
 from imagebytes import *
 from rarbg import *
 from tpb import *
+import os
 
-with open('icon.ico','wb') as f:
+path = '%s\\eliasbenb\\' %  os.environ['APPDATA'] 
+with open(path+'\icon.ico','wb') as f:
     f.write(icon_image_bytes)
 
 app = Tk()
@@ -12,12 +14,7 @@ RARBG_button.place(relx=0.75, rely=0.5, anchor="center", height=200, width=200)
 TPB_button = Button(app, text="TPB", font=("Segoe UI", 15, "bold"), command=tpb)
 TPB_button.place(relx=0.25, rely=0.5, anchor="center", height=200, width=200)
 
-def on_closing():
-    os.remove('icon.ico')
-    app.destroy()
-app.protocol("WM_DELETE_WINDOW", on_closing)
-
 app.title('MagnetMagnet @eliasbenb')
-app.iconbitmap(r'icon.ico')
+app.iconbitmap(path+'icon.ico')
 app.geometry('400x200')
 app.mainloop()

3fe4b07a40f8893cf136fc02ffa82e97ece613f0
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-03-15 18:01:34 +0400

    Made code cleaner

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from tkinter import Tk, messagebox, StringVar, Label, Entry, Button, ttk
-from ImageBytes import *
-from RARBG import *
-from TPB import *
+from imagebytes import *
+from rarbg import *
+from tpb import *
 
 with open('icon.ico','wb') as f:
     f.write(icon_image_bytes)
 
 app = Tk()
-RARBG_button = Button(app, text="RARBG", font=("Segoe UI", 15, "bold"), command=RARBG)
+RARBG_button = Button(app, text="RARBG", font=("Segoe UI", 15, "bold"), command=rarbg)
 RARBG_button.place(relx=0.75, rely=0.5, anchor="center", height=200, width=200)
-TPB_button = Button(app, text="The Pirate Bay", font=("Segoe UI", 15, "bold"), command=TPB)
+TPB_button = Button(app, text="TPB", font=("Segoe UI", 15, "bold"), command=tpb)
 TPB_button.place(relx=0.25, rely=0.5, anchor="center", height=200, width=200)
 
 def on_closing():

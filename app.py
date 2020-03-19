75da24762f6e3671a68f5badd853d2003c915b3d
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-03-19 17:38:23 +0400

    Fixed issue where app wouldn't start

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from rarbg import rarbg
 from tpb import tpb
 import os, webbrowser
 
-path = '%s\\eliasbenb' %  os.environ['APPDATA'] 
+path = '%s\\eliasbenb' %  os.environ['APPDATA']
+os.mkdir(path)
 with open(path+'\\icon.ico','wb') as m1:
     m1.write(icon_image_bytes)
 with open(path+'\\website.png','wb') as m2:

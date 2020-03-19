c95bf58d30f0acf18ae0943dc9f398d1e0dce4a3
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-03-19 15:57:43 +0400

    Cleaned code

--- RARBG.py
+++ RARBG.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import time, os, pyperclip, requests, tkinter.ttk
 import os
 
-path = '%s\\eliasbenb' %  os.environ['APPDATA']
+rarbg_path = '%s\\eliasbenb' %  os.environ['APPDATA']
 
 def rarbg():
     def rarbg_callback():
@@ -21,15 +21,15 @@ def rarbg():
 
         rarbg_clean_soup = rarbg_soup.replace('<', ' ')
         rarbg_clean_soup = rarbg_clean_soup.replace('>', ' ')
-        rarbg_split_soup = rarbg_clean_soup.split(' ')
+        rarbg_clean_soup = rarbg_clean_soup.split(' ')
 
-        rarbg_magnets = str([i for i in rarbg_split_soup if i.startswith('magnet')])
-        rarbg_magnets = rarbg_magnets.replace('magnet:?', '\nmagnet:?')
-        rarbg_magnets = rarbg_magnets.replace("', '", "")
-        rarbg_magnets = rarbg_magnets.replace("['", "")
-        rarbg_magnets = rarbg_magnets.replace("']", "")
-        rarbg_magnets = rarbg_magnets.replace(r"\n", "")
-        rarbg_magnets = "==== Made by @eliasbenb ====" + rarbg_magnets
+        rarbg_clean_soup = str([i for i in rarbg_clean_soup if i.startswith('magnet')])
+        rarbg_clean_soup = rarbg_clean_soup.replace('magnet:?', '\nmagnet:?')
+        rarbg_clean_soup = rarbg_clean_soup.replace("', '", "")
+        rarbg_clean_soup = rarbg_clean_soup.replace("['", "")
+        rarbg_clean_soup = rarbg_clean_soup.replace("']", "")
+        rarbg_clean_soup = rarbg_clean_soup.replace(r"\n", "")
+        rarbg_magnets = "==== Made by @eliasbenb ====" + rarbg_clean_soup
 
         if rarbg_clipboard == "Yes":
             pyperclip.copy(rarbg_magnets)
@@ -37,11 +37,11 @@ def rarbg():
         else:
             pass
         
-        timestr = time.strftime(" %Y%m%d%H%M%S")
-        rarbg_file_name = "RARBG Results " + timestr + ".txt"
-        with open(rarbg_file_name,'w') as r1:
-            for item in rarbg_magnets:
-                r1.write(item)
+        rarbg_timestr = time.strftime(" %Y%m%d%H%M%S")
+        rarbg_file_name = "RARBG Results " + rarbg_timestr + ".txt"
+        with open(rarbg_file_name,'w') as rarbg_w1:
+            for magnet in rarbg_magnets:
+                rarbg_w1.write(magnet)
         print(rarbg_magnets)
 
         messagebox.showinfo("RARBG Scraper @eliasbenb", "Magnet links successfully exported to local directory")
@@ -49,8 +49,8 @@ def rarbg():
     def rarbg_load_config():
         rarbg_domain_entry.delete(0,tkinter.END)
         rarbg_category_entry.delete(0,tkinter.END)
-        with open(path+"\\rarbg_config.env", "r") as r2:
-            rarbg_saved_config = [line.rstrip('\n') for line in r2]
+        with open(rarbg_path+"\\rarbg_config.env", "r") as rarbg_r1:
+            rarbg_saved_config = [line.rstrip('\n') for line in rarbg_r1]
         rarbg_domain_entry.insert(0,rarbg_saved_config[0])
         rarbg_category_entry.insert(0,rarbg_saved_config[1])
         rarbg_clipboard_combobox.insert(0, rarbg_saved_config[2])
@@ -59,8 +59,8 @@ def rarbg():
         rarbg_domain = rarbg_domain_entry.get()
         rarbg_category = rarbg_category_entry.get()
         rarbg_clipboard = rarbg_clipboard_combobox.get()
-        with open(path+"\\rarbg_config.env", "w") as r3:
-            r3.write(rarbg_domain+'\n'+rarbg_category+'\n'+rarbg_clipboard)
+        with open(rarbg_path+"\\rarbg_config.env", "w") as rarbg_w2:
+            rarbg_w2.write(rarbg_domain+'\n'+rarbg_category+'\n'+rarbg_clipboard)
     
     rarbg_app = Tk()
 
@@ -91,7 +91,7 @@ def rarbg():
     rarbg_save_config_button.place(relx=0.8, rely=0.5, anchor="center")
     
     rarbg_app.title('RARBG @eliasbenb')
-    rarbg_app.iconbitmap(path+'\\icon.ico')
+    rarbg_app.iconbitmap(rarbg_path+'\\icon.ico')
     rarbg_app.geometry('500x225')
     
     rarbg_app.mainloop()

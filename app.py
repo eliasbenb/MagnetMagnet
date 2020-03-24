762ed96eeb52a29ed3e19aee00598cfa140bd5b5
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-03-24 20:50:55 +0400

    Added 1377x support

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from tkinter import Button, Label, PhotoImage, Tk
-from imagebytes import *
+from search import search
 from kat import kat
 from rarbg import rarbg
 from tpb import tpb
-from search import search
+from x1377 import x1377
+from imagebytes import *
 import os, webbrowser
 
 path = '%s\\eliasbenb' %  os.environ['APPDATA']
@@ -30,14 +31,16 @@ def github_open():
     webbrowser.open('https://github.com/eliasbenb/MagnetMagnet')
 
 search_button = Button(app, text="Search", font=("Segoe UI", 15, "bold"), command=search)
-search_button.place(relx=(0.5), rely=(75/275), anchor="s", height=75, width=450)
+search_button.place(relx=(0.5), rely=(75/275), anchor="s", height=75, width=600)
 
 kat_button = Button(app, text="KAT", font=("Segoe UI", 15, "bold"), command=kat)
-kat_button.place(relx=(1/6), rely=(225/275), anchor="s", height=150, width=150)
+kat_button.place(relx=(1/4), rely=(225/275), anchor="se", height=150, width=150)
 rarbg_button = Button(app, text="RARBG", font=("Segoe UI", 15, "bold"), command=rarbg)
-rarbg_button.place(relx=(1/2), rely=(225/275), anchor="s", height=150, width=150)
+rarbg_button.place(relx=(2/4), rely=(225/275), anchor="se", height=150, width=150)
 tpb_button = Button(app, text="TPB", font=("Segoe UI", 15, "bold"), command=tpb)
-tpb_button.place(relx=(5/6), rely=(225/275), anchor="s", height=150, width=150)
+tpb_button.place(relx=(3/4), rely=(225/275), anchor="se", height=150, width=150)
+x1377_button = Button(app, text="1377x", font=("Segoe UI", 15, "bold"), command=x1377)
+x1377_button.place(relx=(4/4), rely=(225/275), anchor="se", height=150, width=150)
 
 website_button = Button(app, image=website_photo, command=website_open)
 website_button.place(relx=(1/6), rely=1, anchor="s", height=50, width=32)
@@ -50,6 +53,7 @@ github_button["border"] = "0"
 
 app.title('MagnetMagnet @eliasbenb')
 app.iconbitmap(path+'\\icon.ico')
-app.geometry('450x275')
+app.geometry('600x275')
+app.resizable(False, False)
 
 app.mainloop()
--- tpb.py
+++ tpb.py
@@ -7,13 +7,15 @@ tpb_path = '%s\\eliasbenb' %  os.environ['APPDATA']
 def tpb():
     def tpb_callback():
         tpb_domain = tpb_domain_entry.get()
+        if not tpb_domain.endswith('/'):
+            tpb_domain += '/'
         tpb_category = tpb_category_entry.get()
         tpb_clipboard = tpb_clipboard_combobox.get()
         tpb_link = tpb_domain + 'browse/' + tpb_category
         try:
             tpb_request = requests.get(tpb_link)
         except:
-            messagebox.showinfo("TPB Scraper @eliasbenb", "Something is wrong with the domain/category you inputed.\nMake sure that the domain ends with trailing '/'")
+            messagebox.showinfo("TPB Scraper @eliasbenb", "Something went wrong!")
 
         tpb_source = tpb_request.content
         tpb_soup = BeautifulSoup(tpb_source, 'lxml')
@@ -24,9 +26,9 @@ def tpb():
         
         tpb_timestr = time.strftime(" %Y%m%d%H%M%S")
         tpb_file_name = "TPB Results " + tpb_timestr + ".txt"
-        with open(tpb_file_name,'w') as tpb_w1:
+        with open(tpb_file_name,'w') as w1:
             for magnet in tpb_magnets:
-                tpb_w1.write(magnet)
+                w1.write(magnet)
         messagebox.showinfo("TPB Scraper @eliasbenb", "Magnet links successfully exported to local directory")
 
         if tpb_clipboard == "Yes":
@@ -38,8 +40,8 @@ def tpb():
     def tpb_load_config():
         tpb_domain_entry.delete(0,tkinter.END)
         tpb_category_entry.delete(0,tkinter.END)
-        with open(tpb_path+"\\tpb_config.env", "r") as tpb_r1:
-            tpb_saved_config = [line.rstrip('\n') for line in tpb_r1]
+        with open(tpb_path+"\\tpb_config.env", "r") as r1:
+            tpb_saved_config = [line.rstrip('\n') for line in r1]
         tpb_domain_entry.insert(0,tpb_saved_config[0])
         tpb_category_entry.insert(0,tpb_saved_config[1])
         tpb_clipboard_combobox.insert(0, tpb_saved_config[2])
@@ -48,39 +50,40 @@ def tpb():
         tpb_domain = tpb_domain_entry.get()
         tpb_category = tpb_category_entry.get()
         tpb_clipboard = tpb_clipboard_combobox.get()
-        with open(tpb_path+"\\tpb_config.env", "w") as tpb_w2:
-            tpb_w2.write(tpb_domain+'\n'+tpb_category+'\n'+tpb_clipboard)
+        with open(tpb_path+"\\tpb_config.env", "w") as w2:
+            w2.write(tpb_domain+'\n'+tpb_category+'\n'+tpb_clipboard)
     
     tpb_app = Tk()
 
-    tpb_domain_text = StringVar()
+    tpb_domain_text = StringVar(tpb_app, value="https://tpb.party/")
     tpb_domain_label = Label(tpb_app, text="Enter TPB Domain Link:")
-    tpb_domain_label.place(relx=0.5, rely=0.1, anchor="center")
+    tpb_domain_label.place(relx=(1/2), rely=(1/10), anchor="center")
     tpb_domain_entry = Entry(tpb_app, textvariable=tpb_domain_text)
-    tpb_domain_entry.place(relx=0.5, rely=0.2, anchor="center")
+    tpb_domain_entry.place(relx=(1/2), rely=(1/5), anchor="center", width=300)
 
     tpb_category_text = StringVar()
     tpb_category_label = Label(tpb_app, text="Enter Category String:")
-    tpb_category_label.place(relx=0.5, rely=0.35, anchor="center")
+    tpb_category_label.place(relx=(1/2), rely=(7/20), anchor="center")
     tpb_category_entry = Entry(tpb_app, textvariable=tpb_category_text)
-    tpb_category_entry.place(relx=0.5, rely=0.45, anchor="center")
+    tpb_category_entry.place(relx=(1/2), rely=(9/20), anchor="center")
 
-    tpb_clipboard_label = Label(tpb_app, text="Copy the Magnets to Clipboard?")
-    tpb_clipboard_label.place(relx=0.5, rely=0.6, anchor="center")
-    tpb_clipboard_combobox = ttk.Combobox(tpb_app, values=['Yes', 'No'])
-    tpb_clipboard_combobox.place(relx=0.5, rely=0.7, anchor="center")
+    tpb_clipboard_label = Label(tpb_app, text="Copy to Clipboard?")
+    tpb_clipboard_label.place(relx=(1/2), rely=(3/5), anchor="center")
+    tpb_clipboard_combobox = ttk.Combobox(tpb_app, values=['Yes', 'No'], state='readonly')
+    tpb_clipboard_combobox.place(relx=(1/2), rely=(7/10), anchor="center")
 
-    tpb_ok_button = Button(tpb_app, text = "OK", command = tpb_callback)
-    tpb_ok_button.place(relx=0.5, rely=0.91, anchor="center")
+    tpb_ok_button = Button(tpb_app, text="OK", command=tpb_callback)
+    tpb_ok_button.place(relx=(1/2), rely=(9/10), anchor="center")
 
-    tpb_load_config_button = Button(tpb_app, text = "Load Config", command = tpb_load_config)
-    tpb_load_config_button.place(relx=0.2, rely=0.5, anchor="center")
+    tpb_load_config_button = Button(tpb_app, text ="Load Config", command=tpb_load_config)
+    tpb_load_config_button.place(relx=(1/5), rely=(9/20), anchor="center")
 
-    tpb_save_config_button = Button(tpb_app, text = "Save Config", command = tpb_save_config)
-    tpb_save_config_button.place(relx=0.8, rely=0.5, anchor="center")
+    tpb_save_config_button = Button(tpb_app, text ="Save Config", command=tpb_save_config)
+    tpb_save_config_button.place(relx=(4/5), rely=(9/20), anchor="center")
     
     tpb_app.title('TPB @eliasbenb')
     tpb_app.iconbitmap(tpb_path+'\\icon.ico')
     tpb_app.geometry('500x225')
+    tpb_app.resizable(False, False)
     
     tpb_app.mainloop()

45c931073214e2a7fc73cdd4f393d996ffa1459c
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-03-27 21:09:57 +0400

    Fixed RARBG searcher

--- kat.py
+++ kat.py
@@ -2,92 +2,92 @@ from tkinter import Button, Entry, Label, messagebox, StringVar, Tk, ttk
 import os, pyperclip, requests, re, tkinter.ttk, time
 from bs4 import BeautifulSoup
 
-kat_path = '%s\\eliasbenb' %  os.environ['APPDATA']
+path = '%s\\eliasbenb' %  os.environ['APPDATA']
 
 def kat():
-    def kat_callback():
-        kat_domain = kat_domain_entry.get()
-        if not kat_domain.endswith('/'):
-            kat_domain += '/'
-        kat_category = kat_category_entry.get()
-        kat_clipboard = kat_clipboard_combobox.get()
-        kat_link = kat_domain + kat_category
+    def callback():
+        domain = domain_entry.get()
+        if not domain.endswith('/'):
+            domain += '/'
+        category = category_entry.get()
+        clipboard = clipboard_combobox.get()
+        link = domain + category
         try:
-            kat_request = requests.get(kat_link)
+            request = requests.get(link)
         except:
             messagebox.showinfo("KAT Scraper @eliasbenb", "Something went wrong!")
 
-        kat_source = kat_request.content
-        kat_soup = BeautifulSoup(kat_source, 'lxml')
-        kat_magnets = ['==== Made by @eliasbenb ====']
-        for link in kat_soup.findAll('a', attrs={'href': re.compile("^magnet")}):
-            kat_magnets.append('\n'+link.get('href'))
-        kat_magnets = list(dict.fromkeys(kat_magnets))
+        source = request.content
+        soup = BeautifulSoup(source, 'lxml')
+        magnets = ['==== Made by @eliasbenb ====']
+        for link in soup.findAll('a', attrs={'href': re.compile("^magnet")}):
+            magnets.append('\n'+link.get('href'))
+        magnets = list(dict.fromkeys(magnets))
         
-        kat_timestr = time.strftime(" %Y%m%d%H%M%S")
-        kat_file_name = "KAT Results " + kat_timestr + ".txt"
-        with open(kat_file_name,'w') as w1:
-            for magnet in kat_magnets:
+        timestr = time.strftime(" %Y%m%d%H%M%S")
+        file_name = "KAT Results " + timestr + ".txt"
+        with open(file_name,'w') as w1:
+            for magnet in magnets:
                 w1.write(magnet)
         messagebox.showinfo("KAT Scraper @eliasbenb", "Magnet links successfully exported to local directory")
 
-        if kat_clipboard == "Yes":
-            kat_magnets_clipboard = str(kat_magnets)
-            kat_magnets_clipboard = kat_magnets_clipboard.replace(r"', '\n", "\n")
-            kat_magnets_clipboard = kat_magnets_clipboard.replace("['", "")
-            kat_magnets_clipboard = kat_magnets_clipboard.replace("']", "")
-            pyperclip.copy(kat_magnets_clipboard)
+        if clipboard == "Yes":
+            magnets_clipboard = str(magnets)
+            magnets_clipboard = magnets_clipboard.replace(r"', '\n", "\n")
+            magnets_clipboard = magnets_clipboard.replace("['", "")
+            magnets_clipboard = magnets_clipboard.replace("']", "")
+            pyperclip.copy(magnets_clipboard)
             messagebox.showinfo("KAT Scraper @eliasbenb", "Magnets links successfully copied to clipboard")
         else:
             pass
 
-    def kat_load_config():
-        kat_domain_entry.delete(0,tkinter.END)
-        kat_category_entry.delete(0,tkinter.END)
-        with open(kat_path+"\\kat_config.env", "r") as r1:
-            kat_saved_config = [line.rstrip('\n') for line in r1]
-        kat_domain_entry.insert(0,kat_saved_config[0])
-        kat_category_entry.insert(0,kat_saved_config[1])
-        kat_clipboard_combobox.insert(0, kat_saved_config[2])
+    def load_config():
+        domain_entry.delete(0,tkinter.END)
+        category_entry.delete(0,tkinter.END)
+        with open(path+"\\kat_config.env", "r") as r1:
+            saved_config = [line.rstrip('\n') for line in r1]
+        domain_entry.insert(0,saved_config[0])
+        category_entry.insert(0,saved_config[1])
+        clipboard_combobox.insert(0, saved_config[2])
 
-    def kat_save_config():
-        kat_domain = kat_domain_entry.get()
-        kat_category = kat_category_entry.get()
-        kat_clipboard = kat_clipboard_combobox.get()
-        with open(kat_path+"\\kat_config.env", "w") as w2:
-            w2.write(kat_domain+'\n'+kat_category+'\n'+kat_clipboard)
+    def save_config():
+        domain = domain_entry.get()
+        category = category_entry.get()
+        clipboard = clipboard_combobox.get()
+        with open(path+"\\kat_config.env", "w") as w2:
+            w2.write(domain+'\n'+category+'\n'+clipboard)
     
-    kat_app = Tk()
+    app = Tk()
 
-    kat_domain_text = StringVar(kat_app, value="https://kat.rip/")
-    kat_domain_label = Label(kat_app, text="Enter KAT Domain Link:")
-    kat_domain_label.place(relx=(1/2), rely=(1/10), anchor="center")
-    kat_domain_entry = Entry(kat_app, textvariable=kat_domain_text)
-    kat_domain_entry.place(relx=(1/2), rely=(1/5), anchor="center", width=300)
+    domain_text = StringVar(app, value="https://kat.rip/")
+    domain_label = Label(app, text="Enter KAT Domain Link:")
+    domain_label.place(relx=(1/2), rely=(1/10), anchor="center")
+    domain_entry = Entry(app, textvariable=domain_text)
+    domain_entry.place(relx=(1/2), rely=(1/5), anchor="center", width=300)
 
-    kat_category_text = StringVar()
-    kat_category_label = Label(kat_app, text="Enter Category String:")
-    kat_category_label.place(relx=(1/2), rely=(7/20), anchor="center")
-    kat_category_entry = Entry(kat_app, textvariable=kat_category_text)
-    kat_category_entry.place(relx=(1/2), rely=(9/20), anchor="center")
+    category_text = StringVar()
+    category_label = Label(app, text="Enter Category String:")
+    category_label.place(relx=(1/2), rely=(7/20), anchor="center")
+    category_entry = Entry(app, textvariable=category_text)
+    category_entry.place(relx=(1/2), rely=(9/20), anchor="center")
 
-    kat_clipboard_label = Label(kat_app, text="Copy to Clipboard?")
-    kat_clipboard_label.place(relx=(1/2), rely=(3/5), anchor="center")
-    kat_clipboard_combobox = ttk.Combobox(kat_app, values=['Yes', 'No'], state='readonly')
-    kat_clipboard_combobox.place(relx=(1/2), rely=(7/10), anchor="center")
+    clipboard_label = Label(app, text="Copy to Clipboard?")
+    clipboard_label.place(relx=(1/2), rely=(3/5), anchor="center")
+    clipboard_combobox = ttk.Combobox(app, values=['Yes', 'No'], state='readonly')
+    clipboard_combobox.place(relx=(1/2), rely=(7/10), anchor="center")
 
-    kat_ok_button = Button(kat_app, text="OK", command=kat_callback)
-    kat_ok_button.place(relx=(1/2), rely=(9/10), anchor="center")
+    ok_button = Button(app, text="OK", command=callback)
+    ok_button.place(relx=(1/2), rely=(9/10), anchor="center")
 
-    kat_load_config_button = Button(kat_app, text ="Load Config", command=kat_load_config)
-    kat_load_config_button.place(relx=(1/5), rely=(9/20), anchor="center")
+    load_config_button = Button(app, text ="Load Config", command=load_config)
+    load_config_button.place(relx=(1/5), rely=(9/20), anchor="center")
 
-    kat_save_config_button = Button(kat_app, text ="Save Config", command=kat_save_config)
-    kat_save_config_button.place(relx=(4/5), rely=(9/20), anchor="center")
+    save_config_button = Button(app, text ="Save Config", command=save_config)
+    save_config_button.place(relx=(4/5), rely=(9/20), anchor="center")
     
-    kat_app.title('KAT @eliasbenb')
-    kat_app.iconbitmap(kat_path+'\\icon.ico')
-    kat_app.geometry('500x225')
-    kat_app.resizable(False, False)
+    app.title('KAT @eliasbenb')
+    app.iconbitmap(path+'\\icon.ico')
+    app.geometry('500x225')
+    app.resizable(False, False)
     
-    kat_app.mainloop()
+    app.mainloop()

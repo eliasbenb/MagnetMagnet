3f14263013aca39cab8368fda13bc74bddad7081
Elias Benbourenane <eliasbenbourenane@gmail.com>
2020-03-27 21:09:57 +0400

    Fixed RARBG searcher

--- rarbg.py
+++ rarbg.py
@@ -2,94 +2,91 @@ from tkinter import Button, Entry, Label, messagebox, StringVar, Tk, ttk
 import os, pyperclip, requests, re, tkinter.ttk, time
 from bs4 import BeautifulSoup
 
-rarbg_path = '%s\\eliasbenb' %  os.environ['APPDATA']
+path = '%s\\eliasbenb' %  os.environ['APPDATA']
 
 def rarbg():
-    def rarbg_callback():
-        rarbg_domain = rarbg_domain_entry.get()
-        rarbg_category = rarbg_category_entry.get()
-        rarbg_clipboard = rarbg_clipboard_combobox.get()
-        rarbg_link = rarbg_domain + 'rssdd.php?category=' + rarbg_category
+    def callback():
+        domain = domain_entry.get()
+        if not domain.endswith('/'):
+            domain += '/'
+        category = category_entry.get()
+        clipboard = clipboard_combobox.get()
+        link = domain + 'rssdd.php?category=' + category
         try:
-            rarbg_request = requests.get(rarbg_link)
+            request = requests.get(link)
         except:
-            messagebox.showinfo("RARBG Scraper @eliasbenb", "Something is wrong with the domain/category you inputed.\nMake sure that the domain ends with trailing '/'")
+            messagebox.showinfo("RARBG Scraper @eliasbenb", "Something went wrong!")
 
-        rarbg_source = rarbg_request.content
-        rarbg_soup = str(BeautifulSoup(rarbg_source, 'lxml'))
-
-        rarbg_clean_soup = rarbg_soup.replace('<', ' ')
-        rarbg_clean_soup = rarbg_clean_soup.replace('>', ' ')
-        rarbg_clean_soup = rarbg_clean_soup.split(' ')
-
-        rarbg_clean_soup = str([i for i in rarbg_clean_soup if i.startswith('magnet')])
-        rarbg_clean_soup = rarbg_clean_soup.replace('magnet:?', '\nmagnet:?')
-        rarbg_clean_soup = rarbg_clean_soup.replace("', '", "")
-        rarbg_clean_soup = rarbg_clean_soup.replace("['", "")
-        rarbg_clean_soup = rarbg_clean_soup.replace("']", "")
-        rarbg_clean_soup = rarbg_clean_soup.replace(r"\n", "")
-        rarbg_magnets = "==== Made by @eliasbenb ====" + rarbg_clean_soup
+        source = request.content
+        soup = BeautifulSoup(source, 'xml')
+        magnets = ['==== Made by @eliasbenb ====']
+        for item in soup.findAll('item'):
+            magnets.append('\n'+item.link.text)
         
-        rarbg_timestr = time.strftime(" %Y%m%d%H%M%S")
-        rarbg_file_name = "RARBG Results " + rarbg_timestr + ".txt"
-        with open(rarbg_file_name,'w') as rarbg_w1:
-            for magnet in rarbg_magnets:
-                rarbg_w1.write(magnet)
+        timestr = time.strftime(" %Y%m%d%H%M%S")
+        file_name = "RARBG Results " + timestr + ".txt"
+        with open(file_name,'w') as w1:
+            for magnet in magnets:
+                w1.write(magnet)
         messagebox.showinfo("RARBG Scraper @eliasbenb", "Magnet links successfully exported to local directory")
 
-        if rarbg_clipboard == "Yes":
-            pyperclip.copy(rarbg_magnets)
+        if clipboard == "Yes":
+            magnets_clipboard = str(magnets)
+            magnets_clipboard = magnets_clipboard.replace(r"', '\n", "\n")
+            magnets_clipboard = magnets_clipboard.replace("['", "")
+            magnets_clipboard = magnets_clipboard.replace("']", "")
+            pyperclip.copy(magnets_clipboard)
             messagebox.showinfo("RARBG Scraper @eliasbenb", "Magnets links successfully copied to clipboard")
         else:
             pass
 
-    def rarbg_load_config():
-        rarbg_domain_entry.delete(0,tkinter.END)
-        rarbg_category_entry.delete(0,tkinter.END)
-        with open(rarbg_path+"\\rarbg_config.env", "r") as rarbg_r1:
-            rarbg_saved_config = [line.rstrip('\n') for line in rarbg_r1]
-        rarbg_domain_entry.insert(0,rarbg_saved_config[0])
-        rarbg_category_entry.insert(0,rarbg_saved_config[1])
-        rarbg_clipboard_combobox.insert(0, rarbg_saved_config[2])
+    def load_config():
+        domain_entry.delete(0,tkinter.END)
+        category_entry.delete(0,tkinter.END)
+        with open(path+"\\rarbg_config.env", "r") as r1:
+            saved_config = [line.rstrip('\n') for line in r1]
+        domain_entry.insert(0,saved_config[0])
+        category_entry.insert(0,saved_config[1])
+        clipboard_combobox.insert(0, saved_config[2])
 
-    def rarbg_save_config():
-        rarbg_domain = rarbg_domain_entry.get()
-        rarbg_category = rarbg_category_entry.get()
-        rarbg_clipboard = rarbg_clipboard_combobox.get()
-        with open(rarbg_path+"\\rarbg_config.env", "w") as rarbg_w2:
-            rarbg_w2.write(rarbg_domain+'\n'+rarbg_category+'\n'+rarbg_clipboard)
+    def save_config():
+        domain = domain_entry.get()
+        category = category_entry.get()
+        clipboard = clipboard_combobox.get()
+        with open(path+"\\rarbg_config.env", "w") as w2:
+            w2.write(domain+'\n'+category+'\n'+clipboard)
     
-    rarbg_app = Tk()
+    app = Tk()
 
-    rarbg_domain_text = StringVar(rarbg_app, value="https://rarbgmirror.com/")
-    rarbg_domain_label = Label(rarbg_app, text="Enter RARBG Domain Link:")
-    rarbg_domain_label.place(relx=(1/2), rely=(1/10), anchor="center")
-    rarbg_domain_entry = Entry(rarbg_app, textvariable=rarbg_domain_text)
-    rarbg_domain_entry.place(relx=(1/2), rely=(1/5), anchor="center", width=300)
+    domain_text = StringVar(app, value="https://rarbgmirror.com/")
+    domain_label = Label(app, text="Enter RARBG Domain Link:")
+    domain_label.place(relx=(1/2), rely=(1/10), anchor="center")
+    domain_entry = Entry(app, textvariable=domain_text)
+    domain_entry.place(relx=(1/2), rely=(1/5), anchor="center", width=300)
 
-    rarbg_category_text = StringVar()
-    rarbg_category_label = Label(rarbg_app, text="Enter Category String:")
-    rarbg_category_label.place(relx=(1/2), rely=(7/20), anchor="center")
-    rarbg_category_entry = Entry(rarbg_app, textvariable=rarbg_category_text)
-    rarbg_category_entry.place(relx=(1/2), rely=(9/20), anchor="center")
+    category_text = StringVar()
+    category_label = Label(app, text="Enter Category String:")
+    category_label.place(relx=(1/2), rely=(7/20), anchor="center")
+    category_entry = Entry(app, textvariable=category_text)
+    category_entry.place(relx=(1/2), rely=(9/20), anchor="center")
 
-    rarbg_clipboard_label = Label(rarbg_app, text="Copy to Clipboard?")
-    rarbg_clipboard_label.place(relx=(1/2), rely=(3/5), anchor="center")
-    rarbg_clipboard_combobox = ttk.Combobox(rarbg_app, values=['Yes', 'No'], state='readonly')
-    rarbg_clipboard_combobox.place(relx=(1/2), rely=(7/10), anchor="center")
+    clipboard_label = Label(app, text="Copy to Clipboard?")
+    clipboard_label.place(relx=(1/2), rely=(3/5), anchor="center")
+    clipboard_combobox = ttk.Combobox(app, values=['Yes', 'No'], state='readonly')
+    clipboard_combobox.place(relx=(1/2), rely=(7/10), anchor="center")
 
-    rarbg_ok_button = Button(rarbg_app, text="OK", command=rarbg_callback)
-    rarbg_ok_button.place(relx=(1/2), rely=(9/10), anchor="center")
+    ok_button = Button(app, text="OK", command=callback)
+    ok_button.place(relx=(1/2), rely=(9/10), anchor="center")
 
-    rarbg_load_config_button = Button(rarbg_app, text ="Load Config", command=rarbg_load_config)
-    rarbg_load_config_button.place(relx=(1/5), rely=(9/20), anchor="center")
+    load_config_button = Button(app, text ="Load Config", command=load_config)
+    load_config_button.place(relx=(1/5), rely=(9/20), anchor="center")
 
-    rarbg_save_config_button = Button(rarbg_app, text ="Save Config", command=rarbg_save_config)
-    rarbg_save_config_button.place(relx=(4/5), rely=(9/20), anchor="center")
+    save_config_button = Button(app, text ="Save Config", command=save_config)
+    save_config_button.place(relx=(4/5), rely=(9/20), anchor="center")
     
-    rarbg_app.title('RARBG @eliasbenb')
-    rarbg_app.iconbitmap(rarbg_path+'\\icon.ico')
-    rarbg_app.geometry('500x225')
-    rarbg_app.resizable(False, False)
+    app.title('RARBG @eliasbenb')
+    app.iconbitmap(path+'\\icon.ico')
+    app.geometry('500x225')
+    app.resizable(False, False)
     
-    rarbg_app.mainloop()
+    app.mainloop()

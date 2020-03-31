0f064e461f7a568b8ff227ce13f1df667f3c1303
Elias <eliasbenbourenane@gmail.com>
2020-03-31 12:55:20 +0400

    Completely reworked GUI with PyQt

--- nyaa.py
+++ nyaa.py
@@ -1,21 +1,35 @@
-from tkinter import Button, Entry, Label, messagebox, StringVar, Tk, ttk
-import os, pyperclip, requests, re, tkinter.ttk, time
+from PyQt5 import QtCore, QtGui, QtWidgets
 from bs4 import BeautifulSoup
+import os, requests, re, time
 
 path = '%s\\eliasbenb' %  os.environ['APPDATA']
 
-def nyaa():
-    def callback():
-        domain = domain_entry.get()
-        if not domain.endswith('/'):
-            domain += '/'
-        category = category_entry.get()
-        clipboard = clipboard_combobox.get()
+class Ui_nyaaMainWindow(object):
+    def callback(self):
+        domain = str(self.domainComboBox.currentText())
+        category = str(self.categoryComboBox.currentText())
+
+        if category == "All":
+            category = "0_0"
+        if category == "Anime":
+            category = "1_0"
+        if category == "Audio":
+            category = "2_0"
+        if category == "Literature":
+            category = "3_0"
+        if category == "Live Action":
+            category = "4_0"
+        if category == "Pictures":
+            category = "5_0"
+        if category == "Software":
+            category = "6_0"
+        
         link = domain + '?c=' + category
         try:
             request = requests.get(link)
         except:
-            messagebox.showinfo("Nyaa Scraper @eliasbenb", "Something went wrong!")
+            ErrorMessage = QtWidgets.QErrorMessage()
+            ErrorMessage.showMessage('Something went wrong! Please message me on GitHub!')
 
         source = request.content
         soup = BeautifulSoup(source, 'lxml')
@@ -29,65 +43,69 @@ def nyaa():
         with open(file_name,'w') as w1:
             for magnet in magnets:
                 w1.write(magnet)
-        messagebox.showinfo("Nyaa Scraper @eliasbenb", "Magnet links successfully exported to local directory")
 
-        if clipboard == "Yes":
-            magnets_clipboard = str(magnets)
-            magnets_clipboard = magnets_clipboard.replace(r"', '\n", "\n")
-            magnets_clipboard = magnets_clipboard.replace("['", "")
-            magnets_clipboard = magnets_clipboard.replace("']", "")
-            pyperclip.copy(magnets_clipboard)
-            messagebox.showinfo("Nyaa Scraper @eliasbenb", "Magnets links successfully copied to clipboard")
-        else:
-            pass
+    def setupUi(self, nyaaMainWindow):
+        nyaaMainWindow.setObjectName("nyaaMainWindow")
+        nyaaMainWindow.setFixedSize(600, 330)
+        font = QtGui.QFont()
+        font.setFamily("Bahnschrift Light")
+        nyaaMainWindow.setFont(font)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(path+r"/images/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        nyaaMainWindow.setWindowIcon(icon)
+        self.centralwidget = QtWidgets.QWidget(nyaaMainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.domainComboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.domainComboBox.setGeometry(QtCore.QRect(150, 60, 300, 22))
+        self.domainComboBox.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.domainComboBox.setObjectName("domainComboBox")
+        self.domainComboBox.addItem("")
+        self.domainLabel = QtWidgets.QLabel(self.centralwidget)
+        self.domainLabel.setGeometry(QtCore.QRect(200, 30, 200, 16))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.domainLabel.setFont(font)
+        self.domainLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.domainLabel.setObjectName("domainLabel")
+        self.categoryComboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.categoryComboBox.setGeometry(QtCore.QRect(150, 180, 300, 22))
+        self.categoryComboBox.setObjectName("categoryComboBox")
+        self.categoryComboBox.addItem("")
+        self.categoryComboBox.addItem("")
+        self.categoryComboBox.addItem("")
+        self.categoryComboBox.addItem("")
+        self.categoryComboBox.addItem("")
+        self.categoryComboBox.addItem("")
+        self.categoryComboBox.addItem("")
+        self.categoryLabel = QtWidgets.QLabel(self.centralwidget)
+        self.categoryLabel.setGeometry(QtCore.QRect(200, 150, 200, 16))
+        font = QtGui.QFont()
+        font.setFamily("Bahnschrift Light")
+        font.setPointSize(11)
+        self.categoryLabel.setFont(font)
+        self.categoryLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.categoryLabel.setObjectName("categoryLabel")
+        self.scrapeButton = QtWidgets.QPushButton(self.centralwidget)
+        self.scrapeButton.setGeometry(QtCore.QRect(262, 260, 75, 30))
+        self.scrapeButton.setObjectName("scrapeButton")
+        nyaaMainWindow.setCentralWidget(self.centralwidget)
 
-    def load_config():
-        domain_entry.delete(0,tkinter.END)
-        category_entry.delete(0,tkinter.END)
-        with open(path+"\\nyaa_config.env", "r") as r1:
-            saved_config = [line.rstrip('\n') for line in r1]
-        domain_entry.insert(0,saved_config[0])
-        category_entry.insert(0,saved_config[1])
-        clipboard_combobox.insert(0, saved_config[2])
+        self.scrapeButton.clicked.connect(self.callback)
 
-    def save_config():
-        domain = domain_entry.get()
-        category = category_entry.get()
-        clipboard = clipboard_combobox.get()
-        with open(path+"\\nyaa_config.env", "w") as w2:
-            w2.write(domain+'\n'+category+'\n'+clipboard)
-    
-    app = Tk()
+        self.retranslateUi(nyaaMainWindow)
+        QtCore.QMetaObject.connectSlotsByName(nyaaMainWindow)
 
-    domain_text = StringVar(app, value="https://nyaa.si/")
-    domain_label = Label(app, text="Enter Nyaa Domain Link:")
-    domain_label.place(relx=(1/2), rely=(1/10), anchor="center")
-    domain_entry = Entry(app, textvariable=domain_text)
-    domain_entry.place(relx=(1/2), rely=(1/5), anchor="center", width=300)
-
-    category_text = StringVar()
-    category_label = Label(app, text="Enter Category String:")
-    category_label.place(relx=(1/2), rely=(7/20), anchor="center")
-    category_entry = Entry(app, textvariable=category_text)
-    category_entry.place(relx=(1/2), rely=(9/20), anchor="center")
-
-    clipboard_label = Label(app, text="Copy to Clipboard?")
-    clipboard_label.place(relx=(1/2), rely=(3/5), anchor="center")
-    clipboard_combobox = ttk.Combobox(app, values=['Yes', 'No'], state='readonly')
-    clipboard_combobox.place(relx=(1/2), rely=(7/10), anchor="center")
-
-    ok_button = Button(app, text="OK", command=callback)
-    ok_button.place(relx=(1/2), rely=(9/10), anchor="center")
-
-    load_config_button = Button(app, text ="Load Config", command=load_config)
-    load_config_button.place(relx=(1/5), rely=(9/20), anchor="center")
-
-    save_config_button = Button(app, text ="Save Config", command=save_config)
-    save_config_button.place(relx=(4/5), rely=(9/20), anchor="center")
-    
-    app.title('Nyaa @eliasbenb')
-    app.iconbitmap(path+'\\icon.ico')
-    app.geometry('500x225')
-    app.resizable(False, False)
-    
-    app.mainloop()
+    def retranslateUi(self, nyaaMainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        nyaaMainWindow.setWindowTitle(_translate("nyaaMainWindow", "MagnetMagnet - Nyaa"))
+        self.domainComboBox.setItemText(0, _translate("nyaaMainWindow", "https://nyaa.si/"))
+        self.domainLabel.setText(_translate("nyaaMainWindow", "Choose a Nyaa domain:"))
+        self.categoryComboBox.setItemText(0, _translate("nyaaMainWindow", "All"))
+        self.categoryComboBox.setItemText(1, _translate("nyaaMainWindow", "Anime"))
+        self.categoryComboBox.setItemText(2, _translate("nyaaMainWindow", "Audio"))
+        self.categoryComboBox.setItemText(3, _translate("nyaaMainWindow", "Literature"))
+        self.categoryComboBox.setItemText(4, _translate("nyaaMainWindow", "Live Action"))
+        self.categoryComboBox.setItemText(5, _translate("nyaaMainWindow", "Pictrues"))
+        self.categoryComboBox.setItemText(6, _translate("nyaaMainWindow", "Software"))
+        self.categoryLabel.setText(_translate("nyaaMainWindow", "Choose a category:"))
+        self.scrapeButton.setText(_translate("nyaaMainWindow", "Scrape"))
